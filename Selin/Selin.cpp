08901f6e356b35d4fe603fa3010740a5a098e36e
Senya <arseniy-selin-2004@yandex.ru>
2022-03-21 11:48:28 +0300

    Update

--- Selin/Selin.cpp
+++ Selin/Selin.cpp
@@ -1,11 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int i, j, N, M, Z, s[5];
-    float a[5][5];
+    int i, j, N, M, Z;
+    float a[5][5], s[5], c[5];
 
     cout << "N=";
     cin >> N;
@@ -34,12 +34,25 @@ int main()
 
     cout << "================\n\N. 2:\n";
     for (i = 0; i < N; i++) {
-        s[i] = 0;
         for (j = 0; j < M; j++) {
             if (a[i][j] == 0) {
                 a[i][j] = Z;
             }
             cout << "[" << i + 1 << "]" << "[" << j + 1 << "]: " << a[i][j] << "\n";
         }
+    }
+    
+    cout << "================\n\N. 3:\n";
+    for (i = 0; i < N; i++) { 
+        s[i] = 0;
+        c[i] = 0;
+        for (j = 0; j < M; j++) {
+            if (a[i][j] < Z) {
+                s[i] += a[i][j];
+                c[i] += 1;
+            }
+        }
+        s[i] = s[i] / c[i];
+        cout << "[" << i + 1 << "]: " << s[i] << "\n";
     }
 }
